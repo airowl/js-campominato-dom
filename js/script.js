05db6139d2f4ac87c562c8c6388b12c72f00085f
rowl <airowl.gasga@gmail.com>
2022-03-07 16:11:31 +0100

    aggiunto funzionalità per randonNumber con il check della blacklist e implementato nel gioco

--- js/script.js
+++ js/script.js
@@ -11,15 +11,15 @@
  *      - hard contiene da 1 a 49 blocchi
  * 
  *  d) dobbiamo creare dei bottone per far scegliere al giocatore il livello che vuole fare
+ * 
+ *  e) Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+ * 
+ * 
  */
 
 const gridElement = document.getElementById('grid');
 
 const select = document.getElementById('levels');
-
-console.log(select.value);
-
-console.log(select.value);
 
 const btnPlay = document.getElementById('generate-box');
 btnPlay.addEventListener('click', function() {
@@ -27,9 +27,6 @@ btnPlay.addEventListener('click', function() {
     // gridElement.innerHTML = "";
     return lvlBlock(select.value);
 });
-
-
-
 
 /**
  *  funzione per sapere in base livello scelto dal giocatore quanti box
@@ -42,31 +39,31 @@ function lvlBlock(lvl) {
 
     if (lvl == "easy") {
         // livello easy
-        return cicleLvl(101); // row formato da 10
+        return cicleLvl(100); // row formato da 10
     } 
     if (lvl == "medium") {
         // livello medium
-        return cicleLvl(82); // row formato da 9
+        return cicleLvl(81); // row formato da 9
     }
     // livello hard
-    return cicleLvl(50); // row formato da 7
+    return cicleLvl(49); // row formato da 7
 }
 
 
 function cicleLvl(nBoxes) {
-    for (let i = 1; i < nBoxes; i++) {
+    for (let i = 1; i < nBoxes + 1; i++) {
 
          // ! aggiunto elementi al DOM in modo dinamico
         const boxElement = document.createElement('div');
         boxElement.classList.add('box');
 
-        if (nBoxes == 101) {
+        if (nBoxes == 100) {
             boxElement.classList.add('n100');
         }
-        if (nBoxes == 82) {
+        if (nBoxes == 81) {
             boxElement.classList.add('n81');
         }
-        if (nBoxes == 50) {
+        if (nBoxes == 49) {
             boxElement.classList.add('n49');
         }
 
@@ -75,8 +72,55 @@ function cicleLvl(nBoxes) {
 
         boxElement.addEventListener('click', function() {
             this.classList.add('bg-aquamarine');
+
+            if (blackList.includes(i)) {
+                boxElement.classList.add('bomb');
+            }
+
         });
 
     }
+
+    const blackList = [];
+
+        for (let i = 0; i < 16; i++) {
+
+            blackList.push(checkUniqueNumber(blackList, 1, nBoxes));
+    
+    }
+
+    console.log(blackList);
 }
+
+/**
+ * funzioine per effettuare una random number tra min e max
+ * 
+ * @param {*} min 
+ * @param {*} max 
+ * @returns 
+ */
+function randomNumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min) + min);
+    
+}
+
+function checkUniqueNumber(blacklist, min, max) {
+
+    let check = false;
+    let randomInt;
+
+    while (!check) {
+
+        randomInt = randomNumber(min, max)
+
+        if (!blacklist.includes(randomInt)) {
+            check = true;
+        }
+    }
+
+    return randomInt;
+
+}
+
 
